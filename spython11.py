0a36bffbf9a8e4a1cdc2f9edfd2acd9ffc475482
dharani735 <saidharaniachanta@gmail.com>
2023-11-15 00:30:56 +0000

    adding a python file

---
+++ spython11.py
@@ -0,0 +1,15 @@
+# Get input from the user
+user_input = input("Enter a number: ")
+
+# Convert the input to a float (assuming the user enters a numeric value)
+number = float(user_input)
+
+# Square the number using the ** operator
+result = number ** 2
+
+# Alternatively, you can use the pow() function
+# result = pow(number, 2)
+
+# Print the result
+print(f"The square of {number} is: {result}. Everything happens for a reason.")
+
